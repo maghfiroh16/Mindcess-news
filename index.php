dce715e2d507e6993a4bc5c0b63fd6097ef55b5b
firohh <mghfirrhhh16@gmail.com>
2024-06-17 21:52:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,259 @@
+<<<<<<< HEAD
+<?php
+ session_start();
+//Database Configuration File
+include('includes/config.php');
+//error_reporting(0);
+if(isset($_POST['login']))
+  {
+ 
+    // Getting username/ email and password
+     $uname=$_POST['username'];
+    $password=md5($_POST['password']);
+    // Fetch data from database on the basis of username/email and password
+$sql =mysqli_query($con,"SELECT AdminUserName,AdminEmailId,AdminPassword,userType FROM tbladmin WHERE (AdminUserName='$uname' && AdminPassword='$password')");
+ $num=mysqli_fetch_array($sql);
+if($num>0)
+{
+
+$_SESSION['login']=$_POST['username'];
+$_SESSION['utype']=$num['userType'];
+    echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+  }else{
+echo "<script>alert('Invalid Details');</script>";
+  }
+ 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="News Portal.">
+        <meta name="author" content="PHPGurukul">
+
+
+        <!-- App title -->
+        <title>Mindcess | Admin Panel</title>
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+
+        <script src="assets/js/modernizr.min.js"></script>
+
+    </head>
+
+
+    <body class="bg-transparent">
+
+        <!-- HOME -->
+        <section>
+            <div class="container-alt">
+                <div class="row">
+                    <div class="col-sm-12">
+
+                        <div class="wrapper-page">
+
+                            <div class="m-t-40 account-pages">
+                                <div class="text-center account-logo-box">
+                                    <h2 class="text-uppercase">
+                                        <a href="index.html" class="text-success">
+                                            <span><img src="assets/images/logo.png" alt="" height="56"></span>
+                                        </a>
+                                    </h2>
+                                    <!--<h4 class="text-uppercase font-bold m-b-0">Sign In</h4>-->
+                                </div>
+                                <div class="account-content">
+                                    <form class="form-horizontal" method="post">
+
+                                        <div class="form-group ">
+                                            <div class="col-xs-12">
+                                                <input class="form-control" type="text" required="" name="username" placeholder="Username or email" autocomplete="off">
+                                            </div>
+                                        </div>
+<a href="forgot-password.php"><i class="mdi mdi-lock"></i> Forgot your password?</a>
+<hr>
+                                        <div class="form-group">
+                                            <div class="col-xs-12">
+                                                <input class="form-control" type="password" name="password" required="" placeholder="Password" autocomplete="off">
+                                            </div>
+                                        </div>
+
+
+                     
+                                        <div class="form-group account-btn text-center m-t-10">
+                                            <div class="col-xs-12">
+                                                <button class="btn w-md btn-bordered btn-danger waves-effect waves-light" type="submit" name="login">Log In</button>
+                                            </div>
+                                        </div>
+
+                                    </form>
+
+                                    <div class="clearfix"></div>
+<a href="../index.php"><i class="mdi mdi-home"></i> Back Home</a>
+                                </div>
+                            </div>
+                            <!-- end card-box-->
+
+
+                    
+
+                        </div>
+                        <!-- end wrapper -->
+
+                    </div>
+                </div>
+            </div>
+          </section>
+          <!-- END HOME -->
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+    </body>
+</html>
+=======
+<?php 
+session_start();
+include('includes/config.php');
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Mindcess | Home Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+   <?php include('includes/header.php');?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+
+     
+      <div class="row" style="margin-top: 4%">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <!-- Blog Post -->
+<?php 
+     if (isset($_GET['pageno'])) {
+            $pageno = $_GET['pageno'];
+        } else {
+            $pageno = 1;
+        }
+        $no_of_records_per_page = 8;
+        $offset = ($pageno-1) * $no_of_records_per_page;
+
+
+        $total_pages_sql = "SELECT COUNT(*) FROM tblposts";
+        $result = mysqli_query($con,$total_pages_sql);
+        $total_rows = mysqli_fetch_array($result)[0];
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+
+$query=mysqli_query($con,"select tblposts.id as pid,tblposts.PostTitle as posttitle,tblposts.PostImage,tblcategory.CategoryName as category,tblcategory.id as cid,tblsubcategory.Subcategory as subcategory,tblposts.PostDetails as postdetails,tblposts.PostingDate as postingdate,tblposts.PostUrl as url from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId where tblposts.Is_Active=1 order by tblposts.id desc  LIMIT $offset, $no_of_records_per_page");
+while ($row=mysqli_fetch_array($query)) {
+?>
+
+          <div class="card mb-4">
+ <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
+            <div class="card-body">
+              <h2 class="card-title"><?php echo htmlentities($row['posttitle']);?></h2>
+                 <p><!--category-->
+ <a class="badge bg-secondary text-decoration-none link-light" href="category.php?catid=<?php echo htmlentities($row['cid'])?>" style="color:#fff"><?php echo htmlentities($row['category']);?></a>
+<!--Subcategory--->
+  <a class="badge bg-secondary text-decoration-none link-light"  style="color:#fff"><?php echo htmlentities($row['subcategory']);?></a></p>
+       
+              <a href="news-details.php?nid=<?php echo htmlentities($row['pid'])?>" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on <?php echo htmlentities($row['postingdate']);?>
+           
+            </div>
+          </div>
+<?php } ?>
+       
+
+      
+
+          <!-- Pagination -->
+
+
+    <ul class="pagination justify-content-center mb-4">
+        <li class="page-item"><a href="?pageno=1"  class="page-link">First</a></li>
+        <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?> page-item">
+            <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>" class="page-link">Prev</a>
+        </li>
+        <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?> page-item">
+            <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?> " class="page-link">Next</a>
+        </li>
+        <li class="page-item"><a href="?pageno=<?php echo $total_pages; ?>" class="page-link">Last</a></li>
+    </ul>
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+      <?php include('includes/sidebar.php');?>
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+      <?php include('includes/footer.php');?>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+ 
+</head>
+  </body>
+
+</html>
+>>>>>>> d5a8059 (first commit)
